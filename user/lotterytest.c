96d3a80a0aac9a533313405c21aa80558307a7ef
Farhad Aman <farhad.aman@snapp.cab>
2023-01-27 02:39:36 +0330

    refactor: new lotterytest user application

--- user/lotterytest.c
+++ user/lotterytest.c
@@ -1,44 +1,33 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
-#include "user/user.h"
+#include "user.h"
 #include "kernel/processes_info.h"
+#define LOOP 1326044832
 
+//Avoid optimization
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
 
 void spin() {
-    unsigned x = 0;
-    unsigned y = 0;
-    //int pid = getpid();
-    while (x < 100000) {
-        y = 0;
-        while (y < (10000)) {
-            y++;
-        }
-        x++;
-    }
+    long x = 0;
+    while (x < LOOP) x++;
 }
+
+#pragma GCC pop_options
 
 void printpinfo(int pid)
 {
-	struct processes_info pi;
+	struct processes_info pi = {0};
 	getprocessesinfo(&pi);
 	int i;
-    for (i = 0; i < NPROC; i++) {
-        if(pi.pids[i] == pid) {
-		      printf("Number of tickets that PID %d has: %d\n", pid, pi.tickets[i]);
-	        printf("Number of ticks that PID %d has: %d\n", pid, pi.ticks[i]);
-        }
-    }
-}
-
-void printall()
-{
-  struct processes_info pi;
-  getprocessesinfo(&pi);
-  printf("Number of processes: %d\n", pi.num_processes);
-  int i;
-  for (i = 0; i < NPROC; i++) 
+  for(i = 0; i < NPROC; i++) 
   {
-    printf("PID: %d, Tickets: %d, Ticks: %d\n", pi.pids[i], pi.tickets[i], pi.ticks[i]);
+    if(pi.pids[i] == pid) 
+    {
+      printf("Number of tickets that PID %d has: %d\n", pid, pi.tickets[i]);
+	    printf("Number of ticks that PID %d has: %d\n", pid, pi.ticks[i]);
+      break;
+    }
   }
 }
 
@@ -46,43 +35,67 @@ int
 main(int argc, char *argv[])
 {
     int pid1, pid2, pid3;
-      
-    if ((pid1 = fork()) == 0) 
+    int first;
+
+    settickets(900); 
+
+    // Create 3 processes with different tickets
+    if((pid1 = fork()) == 0) 
     {
-        int pp1 = getpid();
-    	  printf("Process started with PID %d\n\n", pp1);
-        settickets(10);
-        spin();
-	      printpinfo(pp1);
-        printf("Process with PID %d finished!\n\n", pp1);
-        exit(0); 
+      settickets(10);
+      int p1 = getpid();
+	    printf("Process started with PID %d\n\n", p1);
+      spin();
+      printf("Process with PID %d finished!\n\n", p1);
+      printpinfo(p1);
+      exit(0); 
     }
-    else if ((pid2 = fork()) == 0) 
+    else if((pid2 = fork()) == 0) 
     {
-        int pp2 = getpid();
-	      printf("Process started with PID %d\n\n", pp2);
-        settickets(20);
-        spin();
-	      printpinfo(pp2);
-        printf("Process with PID %d finished!\n\n", pp2);
-        exit(0);
+      settickets(20);
+      int p2 = getpid();
+	    printf("Process started with PID %d\n\n", p2);
+      spin();
+      printf("Process with PID %d finished!\n\n", p2);
+      printpinfo(p2);
+      exit(0);
     }
-    else if ((pid3 = fork()) == 0) 
+    else if((pid3 = fork()) == 0) 
     {
-        int pp3 = getpid();
-	      printf("Process started with PID %d\n\n", pp3);
-        settickets(30);
-        spin();
-	      printpinfo(pp3);
-        printf("Process with PID %d finished!\n\n", pp3);
-        exit(0);
+      settickets(30);
+      int p3 = getpid();
+	    printf("Process started with PID %d\n\n", p3);
+      spin();
+      printf("Process with PID %d finished!\n\n", p3);
+      printpinfo(p3);
+      exit(0);
     }
-    //int *a = NULL;
-    //int *b = NULL;
-    //int *c = NULL;
-    wait(0);
-    wait(0);
-    wait(0);
-    printall();
+
+    int pid11 = pid1;
+
+    if((first = wait(&pid11)) > 0 || (first = wait(&pid2)) > 0 || (first = wait(&pid3)) > 0)
+    {
+      if(first == pid1)
+      {
+        printpinfo(pid3);
+        printpinfo(pid2);
+        kill(pid2);
+        kill(pid3);
+      }
+      else if(first == pid2)
+      {
+        printpinfo(pid3);
+        printpinfo(pid1);
+        kill(pid1);
+        kill(pid3);
+      }
+      else
+      {
+        printpinfo(pid2);
+        printpinfo(pid1);
+        kill(pid1);
+        kill(pid2);
+      }
+    }
     exit(0);
 }
